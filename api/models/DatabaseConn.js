50fc6bf1de626a6950121765fd0b29fdbed19250
Breej Vania <breejvania@gmail.com>
2020-08-02 17:58:37 -0300

    Update DatabaseConn.js

--- api/models/DatabaseConn.js
+++ api/models/DatabaseConn.js
@@ -4,57 +4,54 @@ const mysql566 = require('mysql2');
 var Client = require('ssh2').Client;
 var ssh566 = new Client();
 
-var db566 = new Promise(function (resolve, reject) {
+// var db566 = new Promise(function (resolve, reject) {
 
-    // ssh566.on('ready', function () {
+//     // ssh566.on('ready', function () {
 
-    //     ssh566.forwardOut(
-    //         // source address, this can usually be any valid address
-    //         '127.0.0.1',
-    //         // source port, this can be any valid port number
-    //         3306,
-    //         // destination address (localhost here refers to the SSH server)
-    //         'db.cs.dal.ca',
-    //         // destination port
-    //         3306,
-    //         function (err, stream) {
+//     //     ssh566.forwardOut(
+//     //         // source address, this can usually be any valid address
+//     //         '127.0.0.1',
+//     //         // source port, this can be any valid port number
+//     //         3306,
+//     //         // destination address (localhost here refers to the SSH server)
+//     //         'db.cs.dal.ca',
+//     //         // destination port
+//     //         3306,
+//     //         function (err, stream) {
 
-    //             if (err) throw err; // SSH error: can also send error in promise ex. reject(err)
-    //             // use `sql` connection as usual
+//     //             if (err) throw err; // SSH error: can also send error in promise ex. reject(err)
+//     //             // use `sql` connection as usual
 
-    //         });
-    // }).connect({
-    //     host: 'bluenose.cs.dal.ca',
-    //     port: 22,
-    //     user: 'gangi',
-    //     password: 'B00849566'
-    // });
+//     //         });
+//     // }).connect({
+//     //     host: 'bluenose.cs.dal.ca',
+//     //     port: 22,
+//     //     user: 'gangi',
+//     //     password: 'B00849566'
+//     // });
 
-    connection = mysql566.createConnection({
-        host: 'rideshare.clk7cf9g9ru4.us-east-1.rds.amazonaws.com',
-        user: "admin",
-        password: 'Test1234', //TODO: Password in env or separate file
-        port: 3306,
-        database: 'rideshareDB',
-        stream: stream
-
-    });
-
-    // send connection back in variable depending on success or not
-    connection.connect(function (err) {
-        if (err) {
-
-            //resolve(connection);
-            reject(err);
-        } else {
-
-            resolve(connection);
-        }
-    });
+// }).catch((err) => {
+//     if (err) throw "lolll";
+// });
 
 
-}).catch((err) => {
-    if (err) throw "lolll";
+var db566 = mysql566.createConnection({
+    host: 'rideshare.clk7cf9g9ru4.us-east-1.rds.amazonaws.com',
+    user: "admin",
+    password: 'Test1234', //TODO: Password in env or separate file
+    port: 3306,
+    database: 'rideshareDB'
+});
+
+// send connection back in variable depending on success or not
+db566.connect(function (err) {
+    if (err) {
+        console.log("error aai bhai!")
+        //resolve(connection);
+        console.log(err);
+    } else {
+        console.log("connection made to database!!")
+    }
 });
 
 
